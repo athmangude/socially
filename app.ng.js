5cc0a2aa767d27ad517742afeb68342758e60770
Athman Gude <athmangude@gmail.com>
2015-08-20 15:03:47 +0300

    >>Connecting to Meteor for Data >>Fixtures on initial run

--- app.ng.js
+++ app.ng.js
@@ -1,20 +1,36 @@
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient) {
     angular.module('socially', ['angular-meteor']);
 
-    angular.module('socially').controller('PartiesListController', function($scope){
-        $scope.parties = [
-            {
-                name: "Dubstep-Free zone",
-                description: "We no do Dubstep"
-            },
-            {
-                name: "Trance till Dawn",
-                description: "Turn down for what"
-            },
-            {
-                name: "Trance Classics",
-                description: "Classical Trance all night"
+    angular.module('socially').controller('PartiesListController', function($scope, $meteor){
+        $scope.parties = $meteor.collection(Parties);
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.startup(function() {
+        if (Parties.find().count() === 0) {
+            var parties = [
+                {
+                    'name': 'A State of Trance',
+                    'description': '2 hours of the best and the latest in Trance every week'},
+                {
+                    'name': 'A State of Sundays',
+                    'description': 'A selection of hot new Trance by your Favourite DJs as featured in ASOT'},
+                {
+                    'name': 'International Departures',
+                    'description': 'Listen to new uplifting Trance Every two weeks with Myon & Shane 54'
+                },
+                {
+                    'name': 'Who\'s Afraid of 138!',
+                    'description': 'A special episode of ASOT, playing you the harder stuff, exclusively on Digitally Imported'
+                }
+            ];
+
+            for (var i = 0; i < parties.length; i++) {
+                Parties.insert(parties[i]);
             }
-        ]
+        }
     });
 }
