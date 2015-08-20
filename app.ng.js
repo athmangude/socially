25e99f99016b08b1e47f6fa8256d0d9cfd0df852
Athman Gude <athmangude@gmail.com>
2015-08-20 15:26:56 +0300

    >>Adding remove functionality >>Refactxoring for performance

--- app.ng.js
+++ app.ng.js
@@ -5,6 +5,12 @@ if (Meteor.isClient) {
 
     angular.module('socially').controller('PartiesListController', function($scope, $meteor){
         $scope.parties = $meteor.collection(Parties);
+        $scope.remove = function(party){
+            $scope.parties.remove(party);
+        }
+        $scope.removeAll = function(){
+            $scope.parties.remove();
+        }
     });
 }
 
